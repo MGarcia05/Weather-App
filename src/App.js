5642cd83d11b505ab9998f153d1e0a15cc6169d1
Melissa Garcia <megarciade@outlook.com>
2022-03-24 15:59:55 -0400

    Created Weather App
    Used Open Weather API and followed along with Tyler Potts to create weather app. Background images update when weather type changes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+const api = {
+  key: "dfc6be07273ca45d9278e938256c0ed8",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+/* This project was heavily inspired by Tyler Potts and his YouTube video, 
+ "Build a Weather App in React JS | React JS beginner Tutorial". 
+  As of 3/24/2022, the video can be found here: https://www.youtube.com/watch?v=GuA0_Z1llYU*/
+
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}: ${month} ${date}, ${year}`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={(typeof weather.main != "undefined") 
+    ? (
+      (weather.weather[0].main === "Clouds") ? 'app clouds' : 
+      (weather.weather[0].main === "Clear") ? 'app clear' : 
+      (weather.weather[0].main === "Thunderstorm") ? 'app thunder' :
+      (weather.weather[0].main === "Snow") ? 'app snow' :
+      (weather.weather[0].main === "Mist") ? 'app mist' :
+      (weather.weather[0].main === "Rain" || "Drizzle") ? 'app rain' : 'app')
+    : 'app'}> 
+
+      <main>
+        <div className="search-box">
+          <input 
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          onChange={e => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°F
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+        ) : ("") }
+      </main>
     </div>
   );
 }
 
 export default App;
+
+
